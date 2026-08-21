68d1ff3c6c6a05b793186602094e660e19dac22e
Surbhi Mundra <surbhimundra.sm10@gmail.com>
2026-08-22 00:54:35 +0530

    Implement luckyNumbers method in Solution class

---
+++ Day 17/1380.py
@@ -0,0 +1,6 @@
+class Solution:
+    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        row_mins = {min(row) for row in matrix}
+        col_maxs = {max(col) for col in zip(*matrix)}
+        
+        return list(row_mins & col_maxs)
