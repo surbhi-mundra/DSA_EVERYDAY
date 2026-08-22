bb7af5f12787cb0b874e053b3eb137483444184c
Somya Parmar <somyap673@gmail.com>
2026-08-22 21:58:49 +0530

    Add files via upload

---
+++ Day 18/1832.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        boolean[] seen = new boolean[26];
+
+        for(int i = 0; i < sentence.length(); i++) {
+
+            char ch = sentence.charAt(i);
+
+            if(ch >= 'a' && ch <= 'z') {
+                seen[ch - 'a'] = true;
+            }
+        }
+
+    for(int i = 0; i < 26; i++) {
+    if(seen[i] == false)
+        return false;
+    }
+    return true;
+
+    }
+}
+ 
+ 
