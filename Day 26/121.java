373f2855d7ae57485c029003eb512fb4dbb33e6a
Somya Parmar <somyap673@gmail.com>
2026-08-30 23:20:51 +0530

    Add files via upload

---
+++ Day 26/121.java
@@ -0,0 +1,46 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+
+//         int buy = prices[0];
+//         int maxProfit = 0;
+
+//         for (int i = 1; i < prices.length; i++) {
+
+//             if (prices[i] < buy) {
+//                 buy = prices[i];
+//             }
+
+//             int profit = prices[i] - buy;
+
+//             if (profit > maxProfit) {
+//                 maxProfit = profit;
+//             }
+//         }
+
+//         return maxProfit;
+//     }
+// }
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int buy = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+
+            int profit = prices[i] - buy;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
