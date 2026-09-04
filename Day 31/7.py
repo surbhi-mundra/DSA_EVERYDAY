1384de988f95c5ddc90567e02d5efec833b52c83
Surbhi Mundra <surbhimundra.sm10@gmail.com>
2026-09-04 15:31:59 +0530

    Implement reverse integer function in Solution class

---
+++ Day 31/7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        res = int(str(abs(x))[::-1]) * sign
+        
+        if res < -2**31 or res > 2**31 - 1:
+            return 0
+            
+        return res
